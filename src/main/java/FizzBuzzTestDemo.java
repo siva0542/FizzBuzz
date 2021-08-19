3b42a8ec3cb3b7ef22cfa771f78ab732f6b61ed8
siva0542 <sivabhargavakanduru@gmail.com>
2021-08-19 16:20:24 +0530

    Requirement-I: Calculated Fizz or Buzz for the values within the range of 1 to 100

--- src/main/java/FizzBuzzTestDemo.java
+++ src/main/java/FizzBuzzTestDemo.java
@@ -1,2 +1,18 @@
 public class FizzBuzzTestDemo {
+    int value;
+    public FizzBuzzTestDemo(int value){
+        this.value=value;
+    }
+    public String fizzOrBuzz() {
+        if(value>0 && value<101) {
+            if (value % 3 == 0)
+                return "Fizz";
+            else if (value % 5 == 0)
+                return "Buzz";
+            else
+                return "null";
+        }
+        else
+            return "null";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void toCheckNineIsAFizz(){
+        FizzBuzzTestDemo obj=new FizzBuzzTestDemo(9);
+        String actualResult=obj.fizzOrBuzz();
+        String expectedResult="Fizz";
+        assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void toCheckTenIsABuzz(){
+        FizzBuzzTestDemo obj=new FizzBuzzTestDemo(10);
+        String actualResult=obj.fizzOrBuzz();
+        String expectedResult="Buzz";
+        assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void toCheckNineteenIsNotAFizzAndBuzz(){
+        FizzBuzzTestDemo obj=new FizzBuzzTestDemo(19);
+        String actualResult=obj.fizzOrBuzz();
+        String expectedResult="null";
+        assertEquals(actualResult,expectedResult);
+    }
 }
